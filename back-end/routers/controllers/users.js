f5b4a26d9517d14f3d8ce1e0a33958e4c2b36b2f
MuathNahhas <muath.nahhas@yahoo.com>
2021-10-08 14:35:00 +0300

    create addUser

--- back-end/routers/controllers/users.js
+++ back-end/routers/controllers/users.js
@@ -1,6 +1,17 @@
-const connection =require("../../db/db");
-
+const connection = require("../../db/db");
+const bcrypt = require("bcrypt");
 //Add Users
-const addUser=(req,res)=>{
-    const {}=req.body;
-}
+const addUser = async (req, res) => {
+  const { user_name, phone, email, password, payment_ref, role_id } = req.body;
+  const hashPassword = await bcrypt.hash(password, 10);
+  const data = [user_name, phone, email, hashPassword, payment_ref, role_id];
+  const insertAddUser =
+    "INSERT INTO users(user_name, phone, email, password, payment_ref, role_id) VALUES(?,?,?,?,?,?)";
+  connection.query(insertAddUser, data, (err, result) => {
+    if (err) {
+      res.status(404).json({ massage: err });
+    }
+    res.status(201).json({ result: result });
+  });
+};
+module.exports = addUser;
--- back-end/server.js
+++ back-end/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = 5000;
-app.use(express.json())
-require('./db/db')
-const roleRouter=require("./routers/routes/roles");
-const paymentRouter=require("./routers/routes/payment");
+app.use(express.json());
+require("./db/db");
+const roleRouter = require("./routers/routes/roles");
+const paymentRouter = require("./routers/routes/payment");
+const userRouter = require("./routers/routes/users");
 //Routers
-app.use("/role",roleRouter);
-app.use("/payment",paymentRouter);
+app.use("/role", roleRouter);
+app.use("/payment", paymentRouter);
+app.use("/user", userRouter);
 //Routers
 app.listen(PORT, () => {
   console.log(`app listen at ${PORT} `);
