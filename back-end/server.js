8fed0e1e628155585619051339f74aa9bddff1e8
MuathNahhas <muath.nahhas@yahoo.com>
2021-10-08 14:04:57 +0300

    create paymentMethod

--- back-end/server.js
+++ back-end/server.js
@@ -4,8 +4,10 @@ const PORT = 5000;
 app.use(express.json())
 require('./db/db')
 const roleRouter=require("./routers/routes/roles");
+const paymentRouter=require("./routers/routes/payment");
 //Routers
 app.use("/role",roleRouter);
+app.use("/payment",paymentRouter);
 //Routers
 app.listen(PORT, () => {
   console.log(`app listen at ${PORT} `);
