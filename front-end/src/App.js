3dbf54ef76bf3bdcd78b5c18339f49837df9b592
MuathNahhas <muath.nahhas@yahoo.com>
2021-10-12 23:38:14 +0300

    create sidebar component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import StripeContainer from "./component/stripe/StripeContainer";
+import SideBar from "./component/SideBar/SideBar";
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+<SideBar/>
+  </div>;
 }
 
 export default App;
