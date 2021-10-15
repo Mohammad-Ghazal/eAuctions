351312b18489d7efb13d4aef249af83c30896a2a
MuathNahhas <muath.nahhas@yahoo.com>
2021-10-15 21:29:39 +0300

    create component create item

--- front-end/src/component/createItem/CreateItem.js
+++ front-end/src/component/createItem/CreateItem.js
@@ -3,16 +3,26 @@ import "../createItem/CreateItem.css";
 import axios from "axios";
 import { storage } from "../firebase";
 export const CreateItem = () => {
-  const [image, setImage] = useState(null);
+  const [images, setImages] = useState(null); //for complete the fuction of firebase
   const [url, setIUrl] = useState("");
-  const [progress, setProgress] = useState(null);
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [image, setImage] = useState(); //to send name of photo to database
+  const Title = (t) => {
+    setTitle(t.target.value);
+  };
+  const Details = (d) => {
+    setDetails(d.target.value);
+  };
   const handleChange = (e) => {
     if (e.target.files[0]) {
-      setImage(e.target.files[0]);
+      setImages(e.target.files[0]);
+      setImage(e.target.files[0].name);
     }
   };
+  console.log("photoName", image);
   const handelUpload = () => {
-    const uploadTask = storage.ref(`/images/${image.name}`).put(image);
+    const uploadTask = storage.ref(`/images/${images.name}`).put(images);
     uploadTask.on(
       "state_changed",
       (snapshot) => {},
@@ -22,15 +32,21 @@ export const CreateItem = () => {
       () => {
         storage
           .ref("images")
-          .child(image.name)
+          .child(images.name)
           .getDownloadURL()
           .then((url) => {
             setIUrl(url);
+            console.log(url);
           });
       }
     );
+    axios
+      .post("http://localhost:5000/item", { title, details, url })
+      //   { headers: { Authorization: `Bearer ${state.token}` }} use to send token to backend
+      .then((result) => {})
+      .catch((err) => {});
   };
-  console.log("image", image);
+  console.log("image", images);
   return (
     <div>
       <div className="containers">
@@ -38,17 +54,17 @@ export const CreateItem = () => {
         <div className="brand-titles">Create Item</div>
 
         <div className="inputs">
-          {/* <label className="item_label">Title</label>
-          <input className="item_input" type="text" onChange={title} /> */}
+          <label className="item_label">Title</label>
+          <input className="item_input" type="text" onChange={Title} />
           <label>image</label>
           <input className="item_input" type="file" onChange={handleChange} />
-          {/* <label>Details</label>
+          <label>Details</label>
           <textarea
             className="text_input"
             rows="5"
             cols="30"
-            onChange={details}
-          /> */}
+            onChange={Details}
+          />
           <button className="item_button" type="submit" onClick={handelUpload}>
             Submit
           </button>
