35b68a0b909f6f98d0d995bdabe39d07900fdbda
MuathNahhas <muath.nahhas@yahoo.com>
2021-10-15 17:56:51 +0300

    create upload image in direbase

--- front-end/src/component/createItem/CreateItem.js
+++ front-end/src/component/createItem/CreateItem.js
@@ -1,36 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "../createItem/CreateItem.css";
 import axios from "axios";
+import { storage } from "../firebase";
 export const CreateItem = () => {
-  const title = (t) => {
-    t.target.value();
+  const [image, setImage] = useState(null);
+  const [url, setIUrl] = useState("");
+  const [progress, setProgress] = useState(null);
+  const handleChange = (e) => {
+    if (e.target.files[0]) {
+      setImage(e.target.files[0]);
+    }
   };
-  const details = (d) => {
-    d.target.value();
+  const handelUpload = () => {
+    const uploadTask = storage.ref(`/images/${image.name}`).put(image);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {},
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        storage
+          .ref("images")
+          .child(image.name)
+          .getDownloadURL()
+          .then((url) => {
+            setIUrl(url);
+          });
+      }
+    );
   };
-  const submit = () => {
-    axios.post("http://localhost:5000/items",{title,details},)
-  };
+  console.log("image", image);
   return (
     <div>
       <div className="containers">
         <div className="brand-logos"></div>
         <div className="brand-titles">Create Item</div>
+
         <div className="inputs">
-          <label className="item_label">Title</label>
-          <input className="item_input" type="text" onChange={title} />
+          {/* <label className="item_label">Title</label>
+          <input className="item_input" type="text" onChange={title} /> */}
           <label>image</label>
-          <input className="item_input" type="file" />
-          <label>Details</label>
+          <input className="item_input" type="file" onChange={handleChange} />
+          {/* <label>Details</label>
           <textarea
             className="text_input"
             rows="5"
             cols="30"
             onChange={details}
-          />
-          <button className="item_button" type="submit" onClick={submit}>
+          /> */}
+          <button className="item_button" type="submit" onClick={handelUpload}>
             Submit
           </button>
+          <br />
+          {url}
         </div>
       </div>
     </div>
