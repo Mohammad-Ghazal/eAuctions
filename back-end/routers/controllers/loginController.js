be473ed0d7e42fd799ef7267e725f6d5aa79a5ba
ahmadrokasha <ahmadrokasha@gmail.com>
2021-10-16 23:50:23 +0400

    adding captch back end verification

--- back-end/routers/controllers/loginController.js
+++ back-end/routers/controllers/loginController.js
@@ -1,17 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const app = express();
+
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const connection = require("../../db/db");
+// const connection = require("../../db/db");
 require("dotenv").config();
 const { OAuth2Client } = require("google-auth-library");
 const client = new OAuth2Client(
   "787313334015-8ikgfipkm1vi5t5fq9iapgls6urtarns.apps.googleusercontent.com"
 );
-
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 //create login
 const login = async (req, res) => {
   // 1 - login by google part
   //---------------------------------------------------------
 
+  console.log(req.body);
   if (req.headers.authorization) {
     const tokenId = req.headers.authorization.split(" ").pop();
 
@@ -81,11 +92,36 @@ const login = async (req, res) => {
           success: true,
           message: `Email and Password are correct`,
           token: token,
-
         });
       }
     });
   }
 };
 
-module.exports = login;
+//------------------------------------------------------------------
+
+//adding captch backend verification
+
+const CaptchaAuth = (req, res) => {
+  const captch_token = req.headers.authorization.split(" ").pop();
+  const secretkey = "6LfRDdMcAAAAAMmO6g1abMS8pJ-U7nE1MReyXq0N";
+  const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secretkey}&response=${captch_token}`;
+
+  request(url, function (err, response, body) {
+    //the body is the data that contains success message
+    body = JSON.parse(body);
+    res.send("pass");
+
+    // check if the validation failed
+    // if (body.success !== undefined && !data.success) {
+    //   res.send({ success: false, message: "recaptcha failed" });
+    //   return console.log("failed");
+    // }
+
+    // if passed response success message to client
+    //   res.send({ success: true, message: "recaptcha passed" });
+    // });
+  });
+};
+
+module.exports = { login, CaptchaAuth };
