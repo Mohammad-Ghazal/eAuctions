34c0a62ad5e7da7aa74437ad18f1d6b99c8da963
MuathNahhas <muath.nahhas@yahoo.com>
2021-10-17 03:50:03 +0300

    create style of all auction component

--- front-end/src/component/allAuctions/AllAuctions.js
+++ front-end/src/component/allAuctions/AllAuctions.js
@@ -1,36 +1,52 @@
-
-import React,{useState,useEffect} from 'react';
-import { Card } from 'primereact/card';
-import { Button } from 'primereact/button';
+import { Card } from "primereact/card";
+import { Button } from "primereact/button";
+import React, { useState, useEffect } from "react";
 import "../allAuctions/AllAuctions.css";
-import axios from 'axios';
+import axios from "axios";
 export const AllAuctions = () => {
-  const[allAuctions,setAllAuctions]=useState();
-  const[allitem,setAllitem]=useState();
+  const [allAuctions, setAllAuctions] = useState();
 
   useEffect(() => {
     axios.get(`http://localhost:5000/auctions`).then((res) => {
-      console.log(res.data)
-      setAllAuctions(res.data);
+      console.log(res.data.result);
+      setAllAuctions(res.data.result);
     });
   }, []);
-  const header = (
-    <img alt="Card" src="showcase/demo/images/usercard.png" onError={(e) => e.target.src='https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png'} />
-);
-const footer = (
-    <span>
-        <Button label="Save" icon="pi pi-check" />
-        <Button label="Cancel" icon="pi pi-times" className="p-button-secondary p-ml-2" />
-    </span>
-);
 
-return (
+  //   const header = (
+  //     <img alt="Card" src="showcase/demo/images/usercard.png" onError={(e) => e.target.src='https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png'} />
+  // );
+  // const footer = (
+  //     <span>
+  //         <Button label="Show Auction" icon="pi pi-check" />
+
+  //     </span>
+  // );
+
+  return (
     <div className="s">
+      {allAuctions &&
+        allAuctions.map((element, index) => {
+          return (
+            <div key={index}>
+              <Card className="Card_Auction"
+                title={`${element.title}`}
+                subTitle={`Starter Bid : ${element.starter_bid} $`}
+                footer={<Button className="btn_Auction" label="Show Auction" />}
+                header={ 
+                  <img
+                  className="image"
+                    alt="Card"
+                    src={`${element.image}`}
 
-        <Card title="Advanced Card" subTitle="Subtitle" style={{ width: '20em' }} footer={footer} header={header}>
-            <p className="p-m-0" style={{lineHeight: '1.5'}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt
-                quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!</p>
-        </Card>
+                  />
+                }
+              >
+               
+              </Card>
+            </div>
+          );
+        })}
     </div>
-)
+  );
 };
