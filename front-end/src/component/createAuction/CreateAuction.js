39b5bc6e7aa86d057fc278dff832873a5ad00acd
MuathNahhas <muath.nahhas@yahoo.com>
2021-10-18 15:03:40 +0300

    add commit to checkout branch

--- front-end/src/component/createAuction/CreateAuction.js
+++ front-end/src/component/createAuction/CreateAuction.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect} from "react";
 import axios from "axios";
 import "../createAuction/CreateAuction.css";
 export const CreateAuction = () => {
