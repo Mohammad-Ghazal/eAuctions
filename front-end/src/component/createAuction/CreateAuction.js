73390c2a22e98a849ee9d543528919398e500dcd
MuathNahhas <muath.nahhas@yahoo.com>
2021-10-18 15:02:56 +0300

    commit change before you switch branches

--- front-end/src/component/createAuction/CreateAuction.js
+++ front-end/src/component/createAuction/CreateAuction.js
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import axios from "axios";
-
+import "../createAuction/CreateAuction.css";
 export const CreateAuction = () => {
   const [starterBid, setStarterBid] = useState();
-  const [startdate, setStartDate] = useState();
-  const [enddate, setEndDate] = useState();
-  const [bidjump, setBidJump] = useState();
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+  const [bidJump, setBidJump] = useState();
+  const [select, setSelect] = useState();
   const [item, setItem] = useState();
 
   useEffect(() => {
     axios
       .get(`http://localhost:5000/items`, {
         headers: {
-          Authorization: `Bearer ${"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIsInBheW1lbnRSZWYiOm51bGwsInVzZXJOYW1lIjoiTXVhdGggTmFoaGFzIiwiaWF0IjoxNjM0NTA1NDE3LCJleHAiOjE2MzQ1MDkwMTd9.jya2gc8KibZQ0OSnuQ4QjLsOLD3SoEERuQmkcIiA8L4"}`,
+          Authorization: `Bearer ${"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIsInBheW1lbnRSZWYiOm51bGwsInVzZXJOYW1lIjoiTXVhdGggTmFoaGFzIiwiaWF0IjoxNjM0NTIwODUxLCJleHAiOjE2MzQ1MjQ0NTF9.DQR8oLdZTD7rVSYROG3S-JFZIPsQ0ji83mlh4clVeRI"}`,
         },
       })
       .then((res) => {
         setItem(res.data.items);
-        console.log(res.data.items);
       })
       .catch((err) => {
         console.log(err);
@@ -35,27 +35,51 @@ export const CreateAuction = () => {
   const bid_jump = (b) => {
     setBidJump(b.target.value);
   };
-  const click = () => {};
-  return (
-    <div>
-      <h1>create Auction</h1>
-      {item &&
-        item.map((element, index) => {
-          return (
-            <select>
-              <option></option>
-              <option></option>
-              <option></option>
-              <option></option>
-            </select>
-          );
-        })}
+  const handleChange = (e) => {
+    setSelect(e.target.value);
+  };
 
+  const click = () => {
+    axios
+      .post(
+        `http://localhost:5000/auctions`,
+        {
+          starter_bid: starterBid,
+          start_date: startDate,
+          end_date: endDate,
+          bid_jump: bidJump,
+          item_id: select,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIsInBheW1lbnRSZWYiOm51bGwsInVzZXJOYW1lIjoiTXVhdGggTmFoaGFzIiwiaWF0IjoxNjM0NTIwODUxLCJleHAiOjE2MzQ1MjQ0NTF9.DQR8oLdZTD7rVSYROG3S-JFZIPsQ0ji83mlh4clVeRI"}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div className="create_Auction">
+      <h1>create Auction</h1>
+      <select onChange={handleChange}>
+        <option>Please Select Item</option>
+        {item &&
+          item.map((element, index) => {
+            return <option value={element.item_id}>{element.title}</option>;
+          })}
+      </select>
       <input type="number" placeholder="starter_bid" onChange={starter_bid} />
       <input type="date" placeholder="start_date" onChange={start_date} />
       <input type="date" placeholder="end_date" onChange={end_date} />
       <input type="number" placeholder="bid_jump" onChange={bid_jump} />
-      <button value="Submit" onClick={click} />
+      <button className="btn_createAuction" onClick={click}>
+        Submit
+      </button>
     </div>
   );
 };
