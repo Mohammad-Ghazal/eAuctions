cadef19e39b32228f13ead8f5b175ce4035bd04d
MuathNahhas <muath.nahhas@yahoo.com>
2021-10-21 21:31:58 +0300

    change color  of home page

--- front-end/src/component/createAuction/CreateAuction.js
+++ front-end/src/component/createAuction/CreateAuction.js
@@ -1,6 +1,73 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "../createAuction/CreateAuction.css";
+import moment from "moment";
 import "../createItem/CreateItem.css";
 function CreateAuction() {
+  const [starterBid, setStarterBid] = useState();
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+  const [bidJump, setBidJump] = useState();
+  const [select, setSelect] = useState();
+  const [item, setItem] = useState();
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/items`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        console.log(res.data.items)
+        setItem(res.data.items);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  const starter_bid = (s) => {
+    setStarterBid(s.target.value);
+  };
+  const start_date = (t) => {
+    setStartDate(t.target.value);
+  };
+  const end_date = (e) => {
+    setEndDate(e.target.value);
+  };
+  const bid_jump = (b) => {
+    setBidJump(b.target.value);
+  };
+  const handleChange = (e) => {
+    setSelect(e.target.value);
+  };
+
+  const click = () => {
+    console.log("muath", moment(startDate).format("YYYY-MM-DD HH:mm:ss"));
+    axios
+      .post(
+        `http://localhost:5000/auctions`, //date: moment(new Date()).format("YYYY-MM-DD HH:mm:ss")
+        {
+          starter_bid: starterBid,
+          start_date: moment(startDate).format("YYYY-MM-DD HH:mm:ss"),
+          end_date: moment(endDate).format("YYYY-MM-DD HH:mm:ss"),
+          bid_jump: bidJump,
+          item_id: select,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <>
       <div className="Body-FORM">
@@ -9,16 +76,39 @@ function CreateAuction() {
             <img src="./images/a-1.png" />
             <h1>Create Auction</h1>
             <div class="container-form">
-              <select>
+              <select onChange={handleChange}>
                 <option>Please Select Item</option>
+                {item &&
+                  item.map((element, index) => {
+                    return (
+                      <option value={element.item_id}>{element.title}</option>
+                    );
+                  })}
               </select>
-              <input type="number" placeholder="starter_bid" required />
-              <input type="datetime-local" placeholder="start_date" required />
-              <input type="datetime-local" placeholder="end_date" required />
-              <input type="number" placeholder="bid_jump" required />
-
+              <input
+                type="number"
+                placeholder="starter_bid"
+                onChange={starter_bid}
+              />
+              <input
+                type="datetime-local"
+                placeholder="start_date"
+                onChange={start_date}
+              />{" "}
+              <input
+                type="datetime-local"
+                placeholder="end_date"
+                required
+                onChange={end_date}
+              />
+              <input
+                type="number"
+                placeholder="bid_jump"
+                required
+                onChange={bid_jump}
+              />
               <div class="clearfix">
-                <button type="submit" class="signupbtn">
+                <button type="submit" class="signupbtn" onClick={click}>
                   Submit
                 </button>
               </div>
--- front-end/src/component/home/Home.js
+++ front-end/src/component/home/Home.js
@@ -7,14 +7,14 @@ import Section2 from "./section2/Section2";
 import Business from "./business/Business";
 function Home() {
   return (
-    <>
+    <div style={{backgroundColor:"white"}}>
       <Landing />
       <About />
       <Section />
       <FEATURES />
       <Section2 />
       <Business />
-    </>
+    </div>
   );
 }
 export default Home;
