b90bbcf2b81bb8e190e4a909a60cf768c6431074
ahmadrokasha <ahmadrokasha@gmail.com>
2021-10-28 03:53:04 +0400

    add new images for login and register forms

--- front-end/src/component/organization chart/orgchart.js
+++ front-end/src/component/organization chart/orgchart.js
@@ -1,150 +1,314 @@
-import React, { useState } from "react";
-import { OrganizationChart } from "primereact/organizationchart";
-import "./orgchart.css";
+import React from "react";
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
 
-const OrgChart = () => {
-  const [selection, setSelection] = useState([]);
-  const data1 = [
-    {
-      label: "CEO",
-      type: "person",
-      className: "p-person",
-      expanded: true,
-      data: { name: "?????", avatar: "" },
-      children: [
-        {
-          label: "CFO",
-          type: "person",
-          className: "p-person",
-          expanded: true,
-          data: { name: "?????", avatar: "" },
-          children: [
-            {
-              label: "Tax",
-              className: "department-cfo",
-            },
-            {
-              label: "Legal",
-              className: "department-cfo",
-            },
-          ],
-        },
-        {
-          label: "COO",
-          type: "person",
-          className: "p-person",
-          expanded: true,
-          data: { name: "?????", avatar: "" },
-          children: [
-            {
-              label: "Operations",
-              className: "department-coo",
-            },
-          ],
-        },
-        {
-          label: "CTO",
-          type: "person",
-          className: "p-person",
-          expanded: true,
-          data: { name: "?????", avatar: "" },
-          children: [
-            {
-              label: "Development Team",
-              className: "department-cto",
-              expanded: true,
-              children: [
-                {
-                  label: "Full Stack Developer",
-                  className: "p-person",
-                  type: "person",
-                  expanded: true,
-                  data: { name: "Eng. Muath Al Nahhas", avatar: "" },
-                },
-                {
-                  label: "Full Stack Developer",
-                  className: "p-person",
-                  type: "person",
-                  expanded: true,
-                  data: {
-                    name: "Eng. Mohammed Ghazal",
-                  },
-                },
-                {
-                  label: "Full Stack Developer",
-                  className: "p-person img_team",
-                  type: "person",
-                  expanded: true,
-                  data: { name: "Eng. Ahmed Okasha", avatar: "" },
-                },
-              ],
-            },
-            {
-              label: "QA",
-              className: "department-cto",
-            },
-            {
-              label: "R&D",
-              className: "department-cto",
-            },
-          ],
-        },
-      ],
-    },
-  ];
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGoogle,
+  faYoutubeSquare,
+  faLinkedin,
+  faFacebook,
+} from "@fortawesome/free-brands-svg-icons";
 
-  const nodeTemplate = (node) => {
-    if (node.type === "person") {
-      return (
-        <div>
-          <div className="node-header">{node.label}</div>
-          <div className="node-content">
-            <img
-              className={
-                node.data.name === "Eng. Mohammed Ghazal" ||
-                node.data.name === "Eng. Muath Al Nahhas" ||
-                node.data.name === "Eng. Ahmed Okasha"
-                  ? `team`
-                  : ``
-              }
-              alt={node.data.avatar}
-              src={
-                node.data.name === "Eng. Mohammed Ghazal"
-                  ? `https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aHVtYW58ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80`
-                  : `showcase/demo/images/organization/${node.data.avatar}`
-              }
-              onError={(e) =>
-                (e.target.src =
-                  "https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png")
-              }
-              // style={{width:node.data.className==="img_team"?"150px":"",height:node.data.className==="img_team"?"200px":""}}
-            />
-
-            <div>{node.data.name}</div>
-          </div>
-        </div>
-      );
-    }
-
-    if (node.type === "department") {
-      return node.label;
-    }
-  };
-
+function Orgchart() {
   return (
-    <div className="organizationchart-demo">
-      <div className="card">
-        <OrganizationChart
-          value={data1}
-          nodeTemplate={nodeTemplate}
-          selection={selection}
-          selectionMode="multiple"
-          onSelectionChange={(event) => setSelection(event.data)}
-          className="company"
-        ></OrganizationChart>
+    <div style={{ position: "relative", padding: " 10% 20% 20% 20%" }}>
+      <Card.Title className="text-right">
+        <h1>Meet Our Developers !</h1>
+      </Card.Title>
+      <div
+        style={{
+          backgroundColor: "#f0bc54",
+          width: "100%",
+          padding: "5%",
+          transform: "rotate(10deg)",
+        }}
+      >
+        <CardGroup
+          style={{
+            padding: "5%",
+            width: "100%",
+            backgroundColor: "#e6e2ed",
+            borderRadius: "20px",
+            transform: "rotate(-10deg)",
+          }}
+        >
+          <Card
+            style={{
+              height: "55vh",
+
+              border: "none",
+              borderRadius: "20px",
+              boxShadow: "20px 20px 60px #cacaca, -20px -20px 60px #ffffff",
+              background: "linear-gradient(#ebdcce, #fafafa)",
+              margin: "2%",
+            }}
+            text="dark"
+          >
+            <Card.Img
+              style={{
+                background: "linear-gradient(145deg, #d6d6d6, #ffffff)",
+                boxShadow: "20px 20px 60px #cacaca, -20px -20px 60px #ffffff",
+                height: "40%",
+                width: "70%",
+                margin: "10% auto ",
+                borderRadius: "50%",
+              }}
+              variant="top"
+              src="https://firebasestorage.googleapis.com/v0/b/bermuda-e0248.appspot.com/o/images%2F2017-05-15%2006.37.28.jpg?alt=media&token=e54ac234-1a07-444d-8524-f208b960e87b
+"
+            />
+            <Card.Body>
+              <Card.Title>Ahmed Okasha</Card.Title>
+              <Card.Subtitle className="mb-5 text-muted">
+                Web Developer
+              </Card.Subtitle>
+            </Card.Body>
+            <Card.Footer
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <FontAwesomeIcon
+                style={{
+                  color: "#E0655A",
+                  background: "linear-gradient(145deg, #b3a08e, #ffffff)",
+                  borderRadius: "50%",
+                  width: "3rem",
+                  height: "3rem",
+                }}
+                icon={faGoogle}
+                size="3x"
+                border
+              ></FontAwesomeIcon>
+
+              <FontAwesomeIcon
+                style={{
+                  color: "#506BA2",
+                  background: "linear-gradient(145deg, #b3a08e, #ffffff)",
+                  borderRadius: "50%",
+                  width: "3rem",
+                  height: "3rem",
+                }}
+                icon={faFacebook}
+                size="3x"
+                border
+              ></FontAwesomeIcon>
+              <FontAwesomeIcon
+                style={{
+                  color: "#1482BB",
+                  background: "linear-gradient(145deg, #b3a08e, #ffffff)",
+                  borderRadius: "50%",
+                  width: "3rem",
+                  height: "3rem",
+                }}
+                icon={faLinkedin}
+                size="3x"
+                border
+              ></FontAwesomeIcon>
+              <FontAwesomeIcon
+                icon={faYoutubeSquare}
+                size="3x"
+                border
+                style={{
+                  color: "red",
+                  background: "linear-gradient(145deg, #b3a08e, #ffffff)",
+                  borderRadius: "50%",
+                  width: "3rem",
+                  height: "3rem",
+                }}
+              ></FontAwesomeIcon>
+            </Card.Footer>
+          </Card>
+          <Card
+            style={{
+              height: "55vh",
+
+              border: "none",
+              background: "linear-gradient(#ebdcce, #fafafa)",
+
+              borderRadius: "20px",
+              boxShadow: "20px 20px 60px #cacaca, -20px -20px 60px #ffffff",
+
+              margin: "2%",
+            }}
+            text="dark"
+          >
+            <Card.Img
+              style={{
+                background: "linear-gradient(145deg, #d6d6d6, #ffffff)",
+                boxShadow: "20px 20px 60px #cacaca, -20px -20px 60px #ffffff",
+                height: "40%",
+                width: "70%",
+                margin: "10% auto ",
+                borderRadius: "50%",
+              }}
+              variant="top"
+              src="https://firebasestorage.googleapis.com/v0/b/bermuda-e0248.appspot.com/o/images%2Fghazal2.jpg?alt=media&token=1b4e05b0-f2e3-43e7-8bcd-c78a46304591
+
+
+"
+            />
+            <Card.Body>
+              <Card.Title>Mohammed Ghazal</Card.Title>
+              <Card.Subtitle className="mb-5 text-muted">
+                Web Developer
+              </Card.Subtitle>
+            </Card.Body>
+            <Card.Footer
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <FontAwesomeIcon
+                style={{
+                  color: "#E0655A",
+                  background: "linear-gradient(145deg, #b3a08e, #ffffff)",
+                  borderRadius: "50%",
+                  width: "3rem",
+                  height: "3rem",
+                }}
+                icon={faGoogle}
+                size="3x"
+                border
+              ></FontAwesomeIcon>
+
+              <FontAwesomeIcon
+                style={{
+                  color: "#506BA2",
+                  background: "linear-gradient(145deg, #b3a08e, #ffffff)",
+                  borderRadius: "50%",
+                  width: "3rem",
+                  height: "3rem",
+                }}
+                icon={faFacebook}
+                size="3x"
+                border
+              ></FontAwesomeIcon>
+              <FontAwesomeIcon
+                style={{
+                  color: "#1482BB",
+                  background: "linear-gradient(145deg, #b3a08e, #ffffff)",
+                  borderRadius: "50%",
+                  width: "3rem",
+                  height: "3rem",
+                }}
+                icon={faLinkedin}
+                size="3x"
+                border
+              ></FontAwesomeIcon>
+              <FontAwesomeIcon
+                icon={faYoutubeSquare}
+                size="3x"
+                border
+                style={{
+                  color: "red",
+                  background: "linear-gradient(145deg, #b3a08e, #ffffff)",
+                  borderRadius: "50%",
+                  width: "3rem",
+                  height: "3rem",
+                }}
+              ></FontAwesomeIcon>
+            </Card.Footer>
+          </Card>
+          <Card
+            style={{
+              borderRadius: "20px",
+              boxShadow: "20px 20px 60px #cacaca, -20px -20px 60px #ffffff",
+              background: "linear-gradient(#ebdcce, #fafafa)",
+              border: "none",
+
+              margin: "2%",
+
+              height: "55vh",
+            }}
+            text="dark"
+          >
+            <Card.Img
+              style={{
+                background: "linear-gradient(145deg, #d6d6d6, #ffffff)",
+                boxShadow: "20px 20px 60px #cacaca, -20px -20px 60px #ffffff",
+                height: "40%",
+                width: "70%",
+                margin: "10% auto ",
+                borderRadius: "50%",
+              }}
+              variant="top"
+              src="https://firebasestorage.googleapis.com/v0/b/bermuda-e0248.appspot.com/o/images%2Fmuath.jpg?alt=media&token=9c958254-a110-4580-a189-60563f5af3f1
+"
+            />
+            <Card.Body>
+              <Card.Title>Muath Al Nahhas</Card.Title>
+              <Card.Subtitle className="mb-5 text-muted">
+                Web Developer
+              </Card.Subtitle>
+            </Card.Body>
+            <Card.Footer
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <FontAwesomeIcon
+                style={{
+                  color: "#E0655A",
+                  background: "linear-gradient(145deg, #b3a08e, #ffffff)",
+                  borderRadius: "50%",
+                  width: "3rem",
+                  height: "3rem",
+                }}
+                icon={faGoogle}
+                size="3x"
+                border
+              ></FontAwesomeIcon>
+
+              <FontAwesomeIcon
+                style={{
+                  color: "#506BA2",
+                  background: "linear-gradient(145deg, #b3a08e, #ffffff)",
+                  borderRadius: "50%",
+                  width: "3rem",
+                  height: "3rem",
+                }}
+                icon={faFacebook}
+                size="3x"
+                border
+              ></FontAwesomeIcon>
+              <FontAwesomeIcon
+                style={{
+                  color: "#1482BB",
+                  background: "linear-gradient(145deg, #b3a08e, #ffffff)",
+                  borderRadius: "50%",
+                  width: "3rem",
+                  height: "3rem",
+                }}
+                icon={faLinkedin}
+                size="3x"
+                border
+              ></FontAwesomeIcon>
+              <FontAwesomeIcon
+                icon={faYoutubeSquare}
+                size="3x"
+                border
+                style={{
+                  color: "red",
+                  background: "linear-gradient(145deg, #b3a08e, #ffffff)",
+                  borderRadius: "50%",
+                  width: "3rem",
+                  height: "3rem",
+                }}
+              ></FontAwesomeIcon>
+            </Card.Footer>
+          </Card>
+        </CardGroup>
       </div>
     </div>
   );
-};
+}
 
-export default OrgChart;
+export default Orgchart;
